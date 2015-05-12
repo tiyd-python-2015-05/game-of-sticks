7adb16952fa7c5f312a44154eba5d22006264e83
sovello <sovellohpmgani@gmail.com>
2015-05-12 18:05:47 -0400

    start implementation of the play function

--- sticks_game.py
+++ sticks_game.py
@@ -1,29 +1,51 @@
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+
+
+    def __str__(self):
+        return "Player: {}".format(self.name)
+
+
 class Game:
-    def welcome():
+
+    def welcome(self):
         print("\t======WELCOME TO Game of Sticks======\n \
                 Rules of the Game:\n \
                 - Pick any number of sticks 1 through 3\n \
-                - The one to pick the last one is the loser")
+                - The one to pick the last stick is the loser")
 
 
-    def is_integer(num):
+    def is_integer(self, num = 0):
         '''check that the choice is an integer'''
         return isinstance(num, int)
 
 
-    def is_valid_selection(choice, available_sticks):
+    def is_valid_selection(self, choice, available_sticks):
         '''test if the choice of sticks is valid'''
         if choice < available_sticks and 1 <= choice <= 3:
             return True
         else:
             return False
 
-    def remaining_sticks(picked_sticks, initial_sticks):
+
+    def remaining_sticks(self, picked_sticks, initial_sticks):
         return initial_sticks - picked_sticks
 
 
-
+    def play(self):
+        while True:
+            get_sticks = int(input("How many sticks are there on the table (10-100)? "))
+            sticks = int(input("How many sticks do you take? "))
+            if self.is_valid_selection(sticks, get_sticks):
+                if self.remaining_sticks(sticks, get_sticks) == 0:
+                    print("Done")
 
 
 if __name__ == '__main__':
-    welcome()
+    player1 = Player("Sovello")
+    player2 = Player("Hilde")
+
+    game = Game()
+    #game.play()
--- test_sticks_game.py
+++ test_sticks_game.py
@@ -1,21 +1,27 @@
 from sticks_game import Game
+from sticks_game import Player
+
+
 
 
 def test_choice_is_integer():
-    assert is_integer('a4') == False
-    assert is_integer(2.0) == False
-    assert is_integer(183)
+    game = Game()
+    assert game.is_integer('a4') == False
+    assert game.is_integer(2.0) == False
+    assert game.is_integer(183)
 
 
 def test_choice_is_valid():
-    assert is_valid_selection(4, 6) == False
-    assert is_valid_selection(2, 1) == False
-    assert is_valid_selection(0, 3) == False
-    assert is_valid_selection(2, 20)
-    assert is_valid_selection(1, 7)
+    game = Game()
+    assert game.is_valid_selection(4, 6) == False
+    assert game.is_valid_selection(2, 1) == False
+    assert game.is_valid_selection(0, 3) == False
+    assert game.is_valid_selection(2, 20)
+    assert game.is_valid_selection(1, 7)
 
 
 def test_remaining_sticks():
+    game = Game()
     #assert remaining_sticks(picked_sticks, initial_sticks) == 2
-    assert remaining_sticks(2, 3) == 1
-    assert remaining_sticks(4, 10) == 6
+    assert game.remaining_sticks(2, 3) == 1
+    assert game.remaining_sticks(4, 10) == 6
