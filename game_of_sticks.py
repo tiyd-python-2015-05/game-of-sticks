a173db9b53a7722842c34fc2cd64d05242a44c06
John Waldrep <johnwaldrep@gmail.com>
2015-05-12 14:24:55 -0400

    Refactor init, add methods to SticksGame, upload OO-plan.txt; 5/5 passing

--- game_of_sticks.py
+++ game_of_sticks.py
@@ -1,8 +1,28 @@
 
 
 class SticksGame:
-    def __init__(self, sticks=100):
+    def __init__(self, p1, p2, sticks=100):
         self.sticks = sticks
+        self.current_player = p1
+        self.player_a = p1
+        self.player_b = p2
+    def start(self):
+        turn = Turn(self.current_player)
+        self.sticks -= turn.removed_sticks
+    def next_turn(self):
+        if self.current_player == self.player_a:
+            self.current_player = self.player_b
+            turn = Turn(self.current_player)
+            self.sticks -= turn.removed_sticks
+            self.check_win()
+        if self.current_player == self.player_a:
+            self.current_player = self.player_b
+            turn = Turn(self.current_player)
+            self.sticks -= turn.removed_sticks
+            self.check_win()
+    def check_win(self):
+        return True
+
 
 class Player:
     def __init__(self, name='Sam'):
@@ -13,4 +33,6 @@ class Player:
         return something_else
 
 class Turn:
-    pass
+    def __init__(self, player):
+        self.player = player
+        self.removed_sticks = player.choose()
--- test_game_of_sticks.py
+++ test_game_of_sticks.py
@@ -2,11 +2,13 @@ from game_of_sticks import *
 
 
 def test_game_setup():
-    game = SticksGame(sticks=100)
+    p1 = Player(name='Harry')
+    p2 = Player(name='Sally')
+    game = SticksGame(p1, p2, sticks=100)
     assert game.sticks == 100
 
 def test_player_has_name():
-    player1 = Player('Harry')
+    player1 = Player(name='Harry')
     player2 = Player(name='Sally')
     assert player1.name == 'Harry'
     assert player2.name == 'Sally'
@@ -16,6 +18,22 @@ def test_player_can_choose_num_sticks():
     player1 = Player()
     assert player1.choose(auto=3) == 3
 
+class AutoPlayer(Player):
+    def choose(self, auto=3):
+        return auto
 
-def test_turn_removes_sticks():
-    assert False
+def test_first_turn_removes_sticks():
+    p1 = AutoPlayer()
+    p2 = AutoPlayer()
+    game = SticksGame(p1, p2, sticks=100)
+    game.start()
+    assert game.sticks == 97
+
+def test_switch_players():
+    p1 = AutoPlayer()
+    p2 = AutoPlayer()
+    game = SticksGame(p1, p2, sticks=100)
+    assert game.current_player == p1
+    game.start()
+    game.next_turn()
+    assert game.current_player == p2
