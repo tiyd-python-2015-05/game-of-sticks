ff66388671b96239651d774a76624217556a3900
Tripp Shealy <tripp.shealy@gmail.com>
2015-05-12 21:28:55 -0400

    Normal mode working except loop when playing again.

--- sticks.py
+++ sticks.py
@@ -34,7 +34,7 @@ class Game:
         self.winner = self.current_player
 
     def remove_sticks(self, sticks_taken):
-        self.sticks -= sticks_chosen
+        self.sticks = self.sticks - sticks_taken
 
     def start(self):
         while True:
@@ -47,15 +47,13 @@ class Game:
                 self.remove_sticks(sticks_taken)
                 print("There are now {} sticks left".format(self.sticks))
                 self.switch_player()
+                self.game_over_check()
             self.make_winner()
             print("{} wins!".format(self.winner.name))
-            if not self.player1.play_again():
-                breaks
-
-# class Turn:
-#     #Ask the player how many sticks to pickup
-#     #Switches current player
-
+            if self.player1.play_again():
+                self.start() # need to fix - already knows inputs
+            else:
+                break
 
 class Player:
     def __init__(self, name='Bob'):
@@ -70,7 +68,7 @@ class HumanPlayer(Player):
               pick up 1 to 3 sticks. The one who has to pick the final stick
               will be the loser.""")
 
-        players = input("How many players? (pick 2)\>")
+        players = input("How many players? (pick 2)\n>")
 
         try:
             int(players)
@@ -84,21 +82,21 @@ class HumanPlayer(Player):
             print("Invalid Error")
             return self.setup()
 
-    def input_name():
-        input("{}, What is your name?\n> ". format(self.name))
+    def input_name(self):
+        player = input("{}, What is your name?\n> ".format(self.name))
+        return player
 
     def num_of_sticks(self):
-        pick_up_sticks("""{}, How many sticks would you like to pick up?
-                       Please choose between 1 - 3. """.format(self.name))
+        pick_up_sticks = input("{}, How many sticks 1 - 3? ".format(self.name))
 
         try:
-            int(pick_up_sticks)
+            s = int(pick_up_sticks)
 
-            if pick_up_sticks not in range(1, 4):
-                return self.num_of_stick
+            if s not in range(1,4):
+                return self.num_of_sticks()
 
             else:
-                return pick_up_sticks
+                return s
 
         except ValueError:
             print("Invalid Error")
@@ -110,12 +108,12 @@ class HumanPlayer(Player):
             Choose between 10 - 100  """)
 
         try:
-            int(total_sticks)
+            s = int(total_sticks)
 
-            if total_sticks not in range(10,101):
+            if s not in range(10,101):
                 return self.initial_sticks
             else:
-                return total_sticks
+                return s
 
         except ValueError:
             print("Invalid Error")
@@ -124,10 +122,12 @@ class HumanPlayer(Player):
     def play_again(self):
         again = input("Would you like to play again? Y/n? \n>").lower()
 
-        if again is "y":
+        if again == "y":
             return True
         else:
             return False
+        # else:
+        #     return False
     # def player_name():
     #choose 1 or 2 player game
     #     input(What is your)#inputs the number of sticks
@@ -135,8 +135,8 @@ class HumanPlayer(Player):
     #Sets number of sticks to input "How many sticks do you want to pick up"
     #choose to go again
 
-    if __name__ == '__main__':
-        p1 = HumanPlayer("Player1")
-        p2 = HumanPlayer("Player2")
-        game = Game(p1, p2)
-        game.start()
+if __name__ == '__main__':
+    p1 = HumanPlayer("Player1")
+    p2 = HumanPlayer("Player2")
+    game = Game(p1, p2)
+    game.start()
