ee430c56ff3def5e61d79788d50b8374af399ada
Keith Calise <KeithCalise@gmail.com>
2016-02-19 10:13:17 -0500

    updating stick_game_flow

--- game_of_sticks.py
+++ game_of_sticks.py
@@ -1,60 +1,61 @@
 
 class Player:
-
-
+    
     def __init__(self, name):
         self.name = name
-
-
-
-
-
+        
+        
 class Game():
 
     def __init__(self,player1,player2):
         self.player1 = player1
         self.player2 = player2
-        self.sticks = 20
-        #self.switch_players = self.player1
-        self.winner = False
+        self.sticks = 10
+        self.current_player = self.player1
 
 
-    # def switch_players(self):
-    #     if self.current_player == self.player1:
-    #         self.current_player = self.player2
-    #     else:
-    #         self.current_player = self.player1
-            #Explain this logic
+
+
+    def switch_players(self):
+        if self.current_player == self.player1:
+            print("{} it is now your turn".format(self.player1))
+            self.current_player = self.player2
+        else:
+            self.current_player = self.player1
+            print("{} its your turn".format(self.player2))
+        
+
+
     def begin_game(self):
-        while self.sticks > 0:
+        self.switch_players()
+        while self.sticks > 1:
             self.need_choice()
-            #self.switch_players()
-    # def winner(self):
-    #
-    #     if self.sticks == 0:
-    #         return need_choice()
-    #     else:
-    #         self.need_choice()
+            if self.sticks <= 1:
+                print("GAmE OveR! {}, WINS".format(self.current_player))
+            elif self.sticks == 0:
+                print("GAmE OveR! {}, is the Winner".format(self.current_player))
+            else:
+                self.need_choice()
+
 
 
 
     def need_choice(self):
-        self.choice = input('how many sticks(1-3): ')
+        self.choice = input('How many sticks would you like to pick up?(1-3) {}:')
         try:
             int(self.choice)
         except:
-            print('please enter a valid choice')
+            print('Please enter a valid choice(1-3)')
             self.need_choice()
         self.choice = int(self.choice)
         if self.choice > 0 and self.choice <= 3:
             how_many_sticks = self.choice
             self.sticks = self.sticks - how_many_sticks
-        elif self.sticks == 0:
-            print('you lose')
         else:
             print('please enter a valid choice')
             self.need_choice()
-
+        print("There are {} sticks left".format(self.sticks))
+        self.switch_players()
 
 
 if __name__ == '__main__':
